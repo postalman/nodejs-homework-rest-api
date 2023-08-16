37ed45e4a7f7563a9820927bc608b02a5639168b
postalman <sebastianmars78@gmail.com>
2023-08-16 14:26:17 +0300

    Update

--- models/user.js
+++ models/user.js
@@ -34,6 +34,15 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
+    
   },
   { versionKey: false, timestamps: true }
 );
@@ -50,9 +59,14 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+})
+
 const schemas = {
   registerSchema,
   loginSchema,
+  emailSchema,
 };
 
 const User = model("user", userSchema);
